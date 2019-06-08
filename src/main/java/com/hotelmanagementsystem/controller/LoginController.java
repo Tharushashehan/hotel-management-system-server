9ae7794c0a95917e4d3658aad973fa9edfff0494
Sohan Janaka <sohanjanaka101@gmail.com>
2019-06-09 01:50:45 +0530

    login

---
+++ src/main/java/com/hotelmanagementsystem/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.hotelmanagementsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+import com.hotelmanagementsystem.exception.ResourceNotFoundException;
+import com.hotelmanagementsystem.model.User;
+import com.hotelmanagementsystem.repository.UserRepository;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	// Get a Single user
+	@GetMapping("/auth/{email}/{password}")
+	public User getUserById(@PathVariable(value = "email") String userId,
+			@PathVariable(value = "password") String userPassword) {
+		User user = userRepository.findByUserEmail(userId);
+		if (userPassword.equals(String.valueOf(user.getUserPassword()))) {
+			return user;
+		} else {
+			return null;
+		}
+	}
+
+}
--- src/main/java/com/hotelmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/hotelmanagementsystem/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.hotelmanagementsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import com.hotelmanagementsystem.model.User;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository <User, Long> {
+	
+    //SELECT * FROM user WHERE UserEmail = {email}
+    User findByUserEmail(@Param("UserEmail") String UserEmail);
 	
 }
 
