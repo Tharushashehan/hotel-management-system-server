e85452bae2d3ab9778579cc7c1bea1483734c7fb
Sohan Janaka <sohanjanaka101@gmail.com>
2019-06-13 23:52:53 +0530

    login

--- src/main/java/com/hotelmanagementsystem/controller/LoginController.java
+++ src/main/java/com/hotelmanagementsystem/controller/LoginController.java
@@ -1,12 +1,8 @@
 package com.hotelmanagementsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.util.List;
-
-import com.hotelmanagementsystem.exception.ResourceNotFoundException;
+import com.hotelmanagementsystem.model.LoginUser;
 import com.hotelmanagementsystem.model.User;
 import com.hotelmanagementsystem.repository.UserRepository;
 
@@ -19,11 +15,25 @@ public class LoginController {
 
 	// Get a Single user
 	@GetMapping("/auth/{email}/{password}")
-	public User getUserById(@PathVariable(value = "email") String userId,
+	public User getUserById(@PathVariable(value = "email") String userEmail,
 			@PathVariable(value = "password") String userPassword) {
 		System.out.println("login...");
-		User user = userRepository.findByUserEmail(userId);
+		User user = userRepository.findByUserEmail(userEmail);
 		if (userPassword.equals(String.valueOf(user.getUserPassword()))) {
+			System.out.println("pass...");
+			return user;
+		} else {
+			System.out.println("fail...");
+			return null;
+		}
+	}
+	
+	@PostMapping("/authPost")
+	public User getUserByIdP(@RequestParam("userEmail") String userEmail, @RequestParam("userPassword") String userPassword) {
+		System.out.println("email : " + userEmail);
+		System.out.println("password : " + userPassword);
+		User user = userRepository.findByUserEmail(userEmail);
+		if (userPassword.equals(user.getUserPassword())) {
 			System.out.println("pass...");
 			return user;
 		} else {
